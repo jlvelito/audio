79df801a832882d8b1b62cdf68f8c3225f8cff11
Luis Velito <luis@iMac-de-Luis.local>
2016-12-28 14:42:28 -0500

    First steps in design

--- audio/js/min.js
+++ audio/js/min.js
@@ -1,23 +1,30 @@
 $(document).ready(function() {
 
-  var myTime = 0;
+  // var myTime = 0;
+  //
+  // $('.audioCreate').on('click', function() {
+  //   var file = $(this).data('audio')
+  //   mySound = new Audio(file);
+  // });
+  //
+  // $('.audioPlay').on('click', function() {
+  //   mySound.play();
+  // });
+  //
+  // $('.audioPause').on('click', function() {
+  //   mySound.pause();
+  // });
+  //
+  // $('.audioStop').on('click', function() {
+  //   mySound.currentTime = 0;
+  //   mySound.pause();
+  // });
 
-  $('.audioCreate').on('click', function() {
-    var file = $(this).data('audio')
-    mySound = new Audio(file);
-  });
 
-  $('.audioPlay').on('click', function() {
-    mySound.play();
-  });
-
-  $('.audioPause').on('click', function() {
-    mySound.pause();
-  });
-
-  $('.audioStop').on('click', function() {
-    mySound.currentTime = 0;
-    mySound.pause();
+  $('#initial__animation').shuffleLetters({
+    callback: function() {
+      $('#initial').addClass('active');
+    }
   });
 
 });
